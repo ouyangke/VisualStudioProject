9df0c728f5cda2c9e09c940053aae1dd6f147aa8
oyke <oyke@foxmail.com>
2023-08-09 14:40:59 +0800

    删除无用代码

---
+++ CLearn/04常量.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+#define DAY 7
+
+int main(int argc, char const *argv[])
+{
+    cout << "一周有："<< DAY << "天" << endl;
+    system("puase");
+    return 0;
+}
+
+
